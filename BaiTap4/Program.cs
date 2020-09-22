d0beeb041e3b93758379caef5c3936da0f9588ef
HoangLongHotarou <71645795+HoangLongHotarou@users.noreply.github.com>
2020-09-22 22:56:11 +0700

    Revert "Change BaiTap4"
    This reverts commit 3ba924c509749436a01ae452709757fd191a6e68.

--- BaiTap4/Program.cs
+++ BaiTap4/Program.cs
@@ -2,7 +2,6 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 using static System.Console;
 
 namespace BaiTap4
@@ -11,34 +10,31 @@ namespace BaiTap4
     {
         static void Main(string[] args)
         {
-            /*Write an application named DailyTemps that continuously prompts a user for a series
-            of daily high temperatures until the user enters a sentinel value. Valid temperatures
-            range from -20 through 130 Fahrenheit. When the user enters a valid temperature,
-            add it to a total; when the user enters an invalid temperature, display an error message.
-            Before the program ends, display the number of temperatures entered and the average
-            temperature.*/
+            WriteLine("Patrick Barnes");
+            WriteLine("DailyTemps");
+            int count = 0;
+            double sum = 0;
+            double entry = 0;
 
-            const double least = -20, highest = 130;
-            double temperature=0,sum=0, count=0;
-            WriteLine("============ Daily Temps ==============");
-            WriteLine("Enter a temperature (Enter 999 to Exit)");
-            while (temperature!=999)
+            WriteLine("Enter 999 to stop");
+            while (entry != 999)
             {
-                Write("times {0}: ", count + 1);
-                temperature = double.Parse(ReadLine());
-                if (least <= temperature && temperature <= highest)
+                Write("Temp {0}:", count);
+                entry = double.Parse(ReadLine());
+                if (entry >= -20 && entry <= 130)
                 {
-                    sum += temperature;
                     count++;
+                    sum += entry;
                 }
-                else if(temperature!=999)
+                else if (entry != 999)
                 {
-                    WriteLine("Error, Valid temperatures range from - 20 through 130 Fahrenheit");
-                    break;
+                    WriteLine("Error, -20 to 130 . 999 to quit.");
                 }
             }
-            WriteLine("the number of temperatures entered : {0}", count);
-            WriteLine("Average Temperature: {0}", Math.Round((sum/count),2));
+            WriteLine("Count:{0}", count);
+            WriteLine("Avg  :{0}", sum / count);
+
+            WriteLine("--End--");
             ReadKey();
         }
     }
